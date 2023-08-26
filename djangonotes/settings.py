71bbdf84d078d91a131bf141e69f9c8b296b6aac
Bandzile Mohale <138976664+BandziTheVillain@users.noreply.github.com>
2023-08-26 04:56:13 +0200

    Added PG on settings.py

--- djangonotes/settings.py
+++ djangonotes/settings.py
@@ -107,14 +107,24 @@ WSGI_APPLICATION = 'djangonotes.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
-
+else:
+    DATABASES = {
+       'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': os.environ.get("PG_NAME"),
+           'USER': os.environ.get("PG_USER"),
+           'PASSWORD': os.environ.get("PG_PASS"),
+           'HOST': os.environ.get("PG_HOST"),
+           'PORT': os.environ.get("PG_PORT"),
+       }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
